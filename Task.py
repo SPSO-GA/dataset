e1b046c035179cf0199ffdc5c53c5b889c3d0b92
JamesZJS <zhang0512@163.com>
2021-05-06 10:07:03 +0800

    zzz

--- Task.py
+++ Task.py
@@ -11,3 +11,5 @@ class Task(object):
         self.subTask_calculatedAmount = subTask_calculatedAmount
         self.subTask_dependencyRelationship = subTask_dependencyRelationship
         self.subTask_dataDependency = subTask_dataDependency
+
+
